34902adf83ff787e0803e467d7f4788b1e7e120c
NightMachinery <feraidoonmehri@gmail.com>
2024-03-14 12:58:50 +0330

    blackened

--- app.py
+++ app.py
@@ -37,20 +37,20 @@ def getAerials(path):
     return aerialsList
 
 
-def downloadAerial(url: str, file_path: str, name:str):
-    with open(file_path, 'wb') as f:
-        with requests.get(url, stream=True,verify=False) as r:
+def downloadAerial(url: str, file_path: str, name: str):
+    with open(file_path, "wb") as f:
+        with requests.get(url, stream=True, verify=False) as r:
             r.raise_for_status()
-            total = int(r.headers.get('content-length', 0))
+            total = int(r.headers.get("content-length", 0))
 
             # tqdm has many interesting parameters. Feel free to experiment!
             tqdm_params = {
-                'desc': name,
-                'total': total,
-                'miniters': 1,
-                'unit': 'B',
-                'unit_scale': True,
-                'unit_divisor': 1024,
+                "desc": name,
+                "total": total,
+                "miniters": 1,
+                "unit": "B",
+                "unit_scale": True,
+                "unit_divisor": 1024,
             }
             with tqdm.tqdm(**tqdm_params) as pb:
                 for chunk in r.iter_content(chunk_size=8192):
@@ -59,24 +59,29 @@ def downloadAerial(url: str, file_path: str, name:str):
 
 
 def updateSQL():
-    con = sqlite3.connect("/Library/Application Support/com.apple.idleassetsd/Aerial.sqlite")
+    con = sqlite3.connect(
+        "/Library/Application Support/com.apple.idleassetsd/Aerial.sqlite"
+    )
     cur = con.cursor()
     cur.execute("VACUUM;")
     cur.execute("UPDATE ZASSET SET ZLASTDOWNLOADED = 718364962.0204;")
     con.commit()
     con.close()
 
+
 def killService():
-    #idleassetsd
-    subprocess.run(["killall", "idleassetsd"]) 
+    # idleassetsd
+    subprocess.run(["killall", "idleassetsd"])
+
 
 def downloadAerialsParallel(aerial):
-    if 'url-4K-SDR-240FPS' in aerial:
-        url = aerial["url-4K-SDR-240FPS"].replace('\\', '')
-        file_path = aerial_folder_path + aerial["id"] + '.mov'
+    if "url-4K-SDR-240FPS" in aerial:
+        url = aerial["url-4K-SDR-240FPS"].replace("\\", "")
+        file_path = aerial_folder_path + aerial["id"] + ".mov"
         if not os.path.exists(file_path):
             print("Downloading " + aerial["accessibilityLabel"])
             downloadAerial(url, file_path, aerial["accessibilityLabel"])
+
 
 def chooseCategory():
     chosen_category_obj = {}
@@ -88,47 +93,60 @@ def chooseCategory():
 
         categories = []
         i = 0
-        for category in data['categories']:
-            i=i+1
-            print(str(i)+'. '+category['localizedNameKey'].replace('AerialCategory', ''))
-            categories.append(category['localizedNameKey'])
+        for category in data["categories"]:
+            i = i + 1
+            print(
+                str(i)
+                + ". "
+                + category["localizedNameKey"].replace("AerialCategory", "")
+            )
+            categories.append(category["localizedNameKey"])
 
-        categories.append('All')
-        print(str(i+1)+'. All')
+        categories.append("All")
+        print(str(i + 1) + ". All")
 
         choice = input("Enter category number: ")
-        chosen_category = categories[int(choice)-1]
-        if chosen_category != 'All':
+        chosen_category = categories[int(choice) - 1]
+        if chosen_category != "All":
             chosen_category_obj = {}
-            for category in data['categories']:
-                if category['localizedNameKey'] == chosen_category:
+            for category in data["categories"]:
+                if category["localizedNameKey"] == chosen_category:
                     chosen_category_obj = category
                     break
     return chosen_category_obj
+
 
 def chooseSubcategory(categoryObj):
     chosen_subcategory_obj = {}
     with open(json_file_path) as f:
         data = json.load(f)
-        # Get subcategories 
+        # Get subcategories
         subcategories = []
         j = 0
         # Print subcategories
-        print("Select a subcategory in "+categoryObj['localizedNameKey'].replace('AerialCategory', '')+":")
-        for subcat in categoryObj['subcategories']:
-            j=j+1
-            print(str(j)+'. '+subcat['localizedNameKey'].replace('AerialSubcategory', ''))
-            subcategories.append(subcat['localizedNameKey'])
-        
-        subcategories.append('All')
-        print(str(j+1)+'. All')
+        print(
+            "Select a subcategory in "
+            + categoryObj["localizedNameKey"].replace("AerialCategory", "")
+            + ":"
+        )
+        for subcat in categoryObj["subcategories"]:
+            j = j + 1
+            print(
+                str(j)
+                + ". "
+                + subcat["localizedNameKey"].replace("AerialSubcategory", "")
+            )
+            subcategories.append(subcat["localizedNameKey"])
+
+        subcategories.append("All")
+        print(str(j + 1) + ". All")
 
         choice = input("Enter subcategory number: ")
-        chosen_subcategory = subcategories[int(choice)-1]
-        if chosen_subcategory != 'All':
+        chosen_subcategory = subcategories[int(choice) - 1]
+        if chosen_subcategory != "All":
             chosen_subcategory_obj = {}
-            for subcat in categoryObj['subcategories']:
-                if subcat['localizedNameKey'] == chosen_subcategory:
+            for subcat in categoryObj["subcategories"]:
+                if subcat["localizedNameKey"] == chosen_subcategory:
                     chosen_subcategory_obj = subcat
                     break
     return chosen_subcategory_obj
@@ -139,7 +157,7 @@ def chooseAerials():
     subcategoryObj = {}
 
     categoryObj = chooseCategory()
-    if categoryObj!= {}:
+    if categoryObj != {}:
         subcategoryObj = chooseSubcategory(categoryObj)
 
     aerials = getAerials(json_file_path)
@@ -148,20 +166,20 @@ def chooseAerials():
     aerials_set = set()
     for a in aerials:
         if categoryObj == {}:
-            if a['id'] not in aerials_set:
-                aerials_set.add(a['id'])
+            if a["id"] not in aerials_set:
+                aerials_set.add(a["id"])
                 filteredAerials.append(a)
         else:
             for cat in a["categories"]:
-                if cat == categoryObj['id']:
-                    if a['id'] not in aerials_set:
-                        aerials_set.add(a['id'])
+                if cat == categoryObj["id"]:
+                    if a["id"] not in aerials_set:
+                        aerials_set.add(a["id"])
                         filteredAerials.append(a)
-            if subcategoryObj!= {}:
+            if subcategoryObj != {}:
                 for sub in a["subcategories"]:
-                    if sub == subcategoryObj['id']:
-                        if a['id'] not in aerials_set:
-                            aerials_set.add(a['id'])
+                    if sub == subcategoryObj["id"]:
+                        if a["id"] not in aerials_set:
+                            aerials_set.add(a["id"])
                             filteredAerials.append(a)
 
     ic(filteredAerials[0])
@@ -179,14 +197,13 @@ def chooseAerials():
 
     # Filter filteredAerials based on the user's selection
     filteredAerials = [
-        aerial for aerial in filteredAerials
-        if aerial_name(aerial) in selected_aerials
+        aerial for aerial in filteredAerials if aerial_name(aerial) in selected_aerials
     ]
 
-    print("Downloading "+str(len(filteredAerials))+" aerials")
+    print("Downloading " + str(len(filteredAerials)) + " aerials")
 
     # Get the number of download threads from the environment variable
-    download_threads = int(os.environ.get('DOWNLOAD_THREADS', 1))
+    download_threads = int(os.environ.get("DOWNLOAD_THREADS", 1))
 
     with ThreadPoolExecutor(max_workers=download_threads) as executor:
         executor.map(downloadAerialsParallel, filteredAerials)
